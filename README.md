5dd06550ce400d3af27d7de714504496c47ab7d2
emyca <evmyca@gmail.com>
2025-07-07 12:09:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# HMedia
+
+UNDER DEVELOPMENT
+
+It's a Hypermedia-Driven REST demo app (web service) using the Spring HATEOAS project.
+
+
+### Resources
+
+* https://en.wikipedia.org/wiki/HATEOAS
+* https://en.wikipedia.org/wiki/Hypermedia
+* https://spring.io/projects/spring-hateoas
+
+
---
+++ src/main/java/com/example/Spring_Boot_HMedia/HMediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_HMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HMediaApplication.class, args);
+	}
+
+}
