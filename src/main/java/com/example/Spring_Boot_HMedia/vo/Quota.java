1f08f18a2e90f2040787e97efe3a2b0fdde0b1c3
emyca <evmyca@gmail.com>
2025-07-09 13:57:46 +0300

    Add item value object

---
+++ src/main/java/com/example/Spring_Boot_HMedia/vo/Quota.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_HMedia.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Quota {
+
+    private double quota;
+}
