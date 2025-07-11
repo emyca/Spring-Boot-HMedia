0c60e314beabb8a2453d08a870a49dfd25069d2d
emyca <evmyca@gmail.com>
2025-07-11 11:42:15 +0300

    Add item dto request

---
+++ src/main/java/com/example/Spring_Boot_HMedia/dto/ProductDtoRequest.java
@@ -0,0 +1,19 @@
+package com.example.Spring_Boot_HMedia.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductDtoRequest {
+    private long id;
+    private String name;
+    private String measure;
+    private double stock;
+}
