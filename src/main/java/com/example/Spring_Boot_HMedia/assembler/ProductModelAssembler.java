18876b69f61537796b2fa800a7a9b26acb43505c
emyca <evmyca@gmail.com>
2025-07-11 16:09:44 +0300

    Update item model assembler

--- src/main/java/com/example/Spring_Boot_HMedia/assembler/ProductModelAssembler.java
+++ src/main/java/com/example/Spring_Boot_HMedia/assembler/ProductModelAssembler.java
@@ -1,9 +1,14 @@
 package com.example.Spring_Boot_HMedia.assembler;
 
+import com.example.Spring_Boot_HMedia.controller.ProductController;
 import com.example.Spring_Boot_HMedia.entity.Product;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class ProductModelAssembler implements
@@ -13,6 +18,9 @@ public class ProductModelAssembler implements
     public EntityModel<Product> toModel(Product product) {
 
         EntityModel<Product> model = EntityModel.of(product);
+
+        model.add(linkTo(methodOn(ProductController.class)
+                .getAll()).withRel(IanaLinkRelations.COLLECTION));
 
         return model;
     }
