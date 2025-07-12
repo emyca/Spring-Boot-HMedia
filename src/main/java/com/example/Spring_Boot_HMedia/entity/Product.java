7c2e6702d57bbd20eefbfb84decf950e58e60405
emyca <evmyca@gmail.com>
2025-07-12 10:18:05 +0300

    Update item entity

--- src/main/java/com/example/Spring_Boot_HMedia/entity/Product.java
+++ src/main/java/com/example/Spring_Boot_HMedia/entity/Product.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Entity
 @Table(name = "products")
 public class Product extends RepresentationModel<Product> {
