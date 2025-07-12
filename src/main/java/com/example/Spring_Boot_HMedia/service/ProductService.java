03a6c0ba43fb532600c0c1eb013a5f0ebd4f16ba
emyca <evmyca@gmail.com>
2025-07-12 10:37:47 +0300

    Update item service layer

--- src/main/java/com/example/Spring_Boot_HMedia/service/ProductService.java
+++ src/main/java/com/example/Spring_Boot_HMedia/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.Spring_Boot_HMedia.service;
 
+import com.example.Spring_Boot_HMedia.dto.ProductDtoRequest;
 import com.example.Spring_Boot_HMedia.entity.Product;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +8,7 @@ import java.util.List;
 
 @Service
 public interface ProductService {
+    Product save(ProductDtoRequest request);
     List<Product> findAll();
+    Product findById(long id);
 }
--- src/main/java/com/example/Spring_Boot_HMedia/service/ProductServiceImpl.java
+++ src/main/java/com/example/Spring_Boot_HMedia/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.Spring_Boot_HMedia.service;
 
+import com.example.Spring_Boot_HMedia.dto.ProductDtoRequest;
 import com.example.Spring_Boot_HMedia.entity.Product;
 import com.example.Spring_Boot_HMedia.repository.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,30 @@ import java.util.List;
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
+    ProductMapper mapper;
     ProductRepository repository;
 
-    public ProductServiceImpl(ProductRepository repository) {
+    public ProductServiceImpl(
+            ProductRepository repository,
+            ProductMapper mapper
+    ) {
         this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Product save(ProductDtoRequest request) {
+        return repository.save(mapper.dtoCreateToEntity(request));
     }
 
     @Override
     public List<Product> findAll() {
         return repository.findAll();
+    }
+
+    @Override
+    public Product findById(long id) {
+        return repository.findById(id).orElse(null);
     }
 
 }
