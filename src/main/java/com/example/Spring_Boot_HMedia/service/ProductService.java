489e015847dbea6fa548250ad73bc4ca29d51454
emyca <evmyca@gmail.com>
2025-07-16 11:51:02 +0300

    Update base service

--- src/main/java/com/example/Spring_Boot_HMedia/service/ProductService.java
+++ src/main/java/com/example/Spring_Boot_HMedia/service/ProductService.java
@@ -12,4 +12,5 @@ public interface ProductService {
     List<Product> findAll();
     Product findById(long id);
     Product updateById(long id, ProductDtoRequest request);
+    Product accept(double quota, long id);
 }
