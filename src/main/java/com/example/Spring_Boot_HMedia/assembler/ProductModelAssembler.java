f7b8454b2b9a435ab0f56c5e3759a854b6a84326
emyca <evmyca@gmail.com>
2025-07-17 09:32:33 +0300

    Fix item model assembler

--- src/main/java/com/example/Spring_Boot_HMedia/assembler/ProductModelAssembler.java
+++ src/main/java/com/example/Spring_Boot_HMedia/assembler/ProductModelAssembler.java
@@ -5,6 +5,7 @@ import com.example.Spring_Boot_HMedia.entity.Product;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -15,7 +16,8 @@ public class ProductModelAssembler implements
         RepresentationModelAssembler<Product, EntityModel<Product>> {
 
     @Override
-    public EntityModel<Product> toModel(Product product) {
+    @NonNull
+    public EntityModel<Product> toModel(@NonNull Product product) {
 
         EntityModel<Product> model = EntityModel.of(product);
 
