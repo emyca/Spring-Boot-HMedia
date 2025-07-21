283088061bf7456a62a1b10790d261b8fefd64e9
emyca <evmyca@gmail.com>
2025-07-21 16:28:37 +0300

    Update item service test

--- src/test/java/com/example/Spring_Boot_HMedia/service/ProductServiceImplTest.java
+++ src/test/java/com/example/Spring_Boot_HMedia/service/ProductServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.Spring_Boot_HMedia.service;
 
+import com.example.Spring_Boot_HMedia.dto.ProductDtoRequest;
 import com.example.Spring_Boot_HMedia.entity.Product;
 import com.example.Spring_Boot_HMedia.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -16,10 +17,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
+
+    @Mock
+    ProductMapper mapper;
 
     @Mock
     ProductRepository repository;
@@ -28,18 +32,40 @@ class ProductServiceImplTest {
     ProductServiceImpl service;
 
     @Test
+    @DisplayName("Gets proper product saving it.")
+    void whenSaveProduct_thenThoseProductReturned() {
+        // Given
+        // Mocks item to save
+        ProductDtoRequest productDtoRequest = new ProductDtoRequest(null, "banana", "kg", 15);
+        Product productToSave = new Product(null, "banana", "kg", 15);
+        Product productSaved = new Product(1L, "banana", "kg", 15);
+        // When
+        // Mocks mapper, repository and service methods calls
+        when(mapper.dtoCreateToEntity(productDtoRequest)).thenReturn(productToSave);
+        when(repository.save(productToSave)).thenReturn(productSaved);
+        Product result = service.save(productDtoRequest);
+        // Then
+        // Asserts/verify that mocked method call has been invoked
+        verify(repository, times(1)).save(productToSave);
+        // Asserts that item is not null
+        assertNotNull(result);
+        // Asserts item to save and item returned are the same
+        assertEquals(result, productSaved);
+    }
+
+    @Test
     @DisplayName("Gets saved products collection that has its proper quantity.")
     void givenSavedProducts_whenFetchAll_thenGetAll() {
         // Given
         // Mocks previously populated items
-        Product product1 = new Product( 1L, "banana", "kg", 15);
-        Product product2 = new Product( 2L, "mango", "pcs.", 23);
-        Product product3 = new Product( 3L, "kiwi", "box", 19);
+        Product product1 = new Product(1L, "banana", "kg", 15);
+        Product product2 = new Product(2L, "mango", "pcs.", 23);
+        Product product3 = new Product(3L, "kiwi", "box", 19);
         // When
-        // Calls repository and service methods
+        // Mocks repository and service methods calls
         given(repository.findAll())
-                .willReturn(List.of( product1, product2, product3 ));
-        var  productList = service.findAll();
+                .willReturn(List.of(product1, product2, product3));
+        var productList = service.findAll();
         // Then
         // Asserts that items list is not null
         assertThat(productList).isNotNull();
@@ -56,9 +82,9 @@ class ProductServiceImplTest {
         long productId = 1L;
         Product mockProduct = new Product(productId, "banana", "kg", 15);
         // When
-        // Calls repository and service methods
+        // Mocks repository and service methods calls
         when(repository.findById(productId)).thenReturn(Optional.of(mockProduct));
-        Product result = service.findById(productId);
+        var result = service.findById(productId);
         // Asserts that item is not null
         assertNotNull(result);
         // Asserts that retrieved item is proper item
