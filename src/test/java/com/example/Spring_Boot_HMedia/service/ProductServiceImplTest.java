8bdea13b5f4d1af1682c34ffe5c14a1c822c1dd4
emyca <evmyca@gmail.com>
2025-07-21 15:06:46 +0300

    Update item service unit test

--- src/test/java/com/example/Spring_Boot_HMedia/service/ProductServiceImplTest.java
+++ src/test/java/com/example/Spring_Boot_HMedia/service/ProductServiceImplTest.java
@@ -10,9 +10,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
@@ -27,12 +31,12 @@ class ProductServiceImplTest {
     @DisplayName("Gets saved products collection that has its proper quantity.")
     void givenSavedProducts_whenFetchAll_thenGetAll() {
         // Given
-        // Previously populated items
+        // Mocks previously populated items
         Product product1 = new Product( 1L, "banana", "kg", 15);
         Product product2 = new Product( 2L, "mango", "pcs.", 23);
         Product product3 = new Product( 3L, "kiwi", "box", 19);
         // When
-        // Calls of repository and service methods
+        // Calls repository and service methods
         given(repository.findAll())
                 .willReturn(List.of( product1, product2, product3 ));
         var  productList = service.findAll();
@@ -42,6 +46,23 @@ class ProductServiceImplTest {
         // Asserts that the items list has previously populated items
         // and has specific items quantity
         assertThat(productList.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Gets saved product by id properly.")
+    public void whenGetById_thenProperProductReturned() {
+        // Given
+        // Mocks previously saved item
+        long productId = 1L;
+        Product mockProduct = new Product(productId, "banana", "kg", 15);
+        // When
+        // Calls repository and service methods
+        when(repository.findById(productId)).thenReturn(Optional.of(mockProduct));
+        Product result = service.findById(productId);
+        // Asserts that item is not null
+        assertNotNull(result);
+        // Asserts that retrieved item is proper item
+        assertEquals(mockProduct, result);
     }
 
 }
